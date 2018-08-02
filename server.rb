2c9b4224c17f740fe5703bef10216fedb6efef3c
Billy Davidson <billy.davids52@gmail.com>
2018-08-02 17:47:00 -0400

    my first app complete

--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/home' do
   erb :index      # render any file from this line
 end
@@ -9,5 +17,18 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['guitar', 'ping pong', 'cooking', 'singing in the shower']
+  @interests = ['animals', 'music', 'movies', 'theatre', 'nature']
   erb :bespectacled
+end
+
+get '/favourites' do
+  @fav_links = [
+    "https:/www.pensionplanpuppets.com/",
+    "https:/www.bluebirdbanter.com/",
+    "https:/www.raptorshq.com/",
+    "https:/stackoverflow.com/",
+    "https:/youtube.com/"
+  ]
+  erb :cool_links
 end
